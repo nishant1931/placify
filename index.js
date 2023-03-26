b0177eb4e7041705b3282ca51e98c77ba74cbf4c
nishant1931 <nishoo1902@gmail.com>
2023-03-26 20:03:08 +0530

    change host

--- index.js
+++ index.js
@@ -66,9 +66,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("DATABASE CONNECTED...");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("SERVER STARTED...");
+    });
   })
   .catch((err) => console.log(err));
-
-app.listen(process.env.PORT || 5000, () => {
-  console.log("SERVER STARTED...");
-});
